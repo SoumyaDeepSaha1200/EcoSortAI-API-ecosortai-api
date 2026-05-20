df71dcfef5e46ca67910be4714bc5384b886d8a7
Soumya Deep Saha <deepadiotic@gmail.com>
2026-05-20 10:58:13 +0530

    Prepare EcoSortAI API for Render deployment

--- api_server.py
+++ api_server.py
@@ -188,9 +188,11 @@ if __name__ == "__main__":
     load_models()
 
     print("🌐 API running at http://127.0.0.1:5001")
+    port = int(os.environ.get("PORT", 5001))
+
     app.run(
         host="0.0.0.0",
-        port=port=int(os.environ.get("PORT", 5001)),
-        debug=False,        # IMPORTANT
-        use_reloader=False # CRITICAL FOR WINDOWS + TF
+        port=port,
+        debug=False,
+        use_reloader=False
     )
